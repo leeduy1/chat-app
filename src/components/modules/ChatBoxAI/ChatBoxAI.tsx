5955460e0c0f0676d885e5a3fdd5798b8e73a4f3
leeduy1 <levanduy.gv2003@gmail.com>
2025-05-07 23:22:36 +0700

    feat: add text

--- src/components/modules/ChatBoxAI/ChatBoxAI.tsx
+++ src/components/modules/ChatBoxAI/ChatBoxAI.tsx
@@ -114,7 +114,9 @@ const ChatBoxAI: React.FC = () => {
                     sentTime: message.sentTime,
                     sender: message.sender,
                     direction:
-                      message.sender === 'ChatGPT' ? 'incoming' : 'outgoing',
+                      message.sender === 'ChatGPT Bot1'
+                        ? 'incoming'
+                        : 'outgoing',
                     position:
                       message.sender === 'ChatGPT' ? 'normal' : 'single',
                   }}
